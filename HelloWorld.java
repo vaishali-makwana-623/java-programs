b7c9bee48bce84f18d3415ed97769ed69ebd8f8d
Vaishali Makwana <vaishalimakwana200725@gmail.com>
2026-08-10 14:14:09 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// Simple Hello World Program
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to my Java Programming Repository.");
+    }
+}
