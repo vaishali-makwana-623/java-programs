7235ccd2293e75987dcd0756e1e2190ac8ef6cea
Vaishali Makwana <vaishalimakwana200725@gmail.com>
2026-08-11 14:14:53 +0530

    Create StudentGrade.java

---
+++ StudentGrade.java
@@ -0,0 +1,29 @@
+// Program to calculate student grade based on marks
+
+import java.util.Scanner;
+
+public class StudentGrade {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter marks: ");
+        int marks = sc.nextInt();
+
+        if (marks >= 90) {
+            System.out.println("Grade: A+");
+        } else if (marks >= 80) {
+            System.out.println("Grade: A");
+        } else if (marks >= 70) {
+            System.out.println("Grade: B");
+        } else if (marks >= 60) {
+            System.out.println("Grade: C");
+        } else if (marks >= 50) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F");
+        }
+
+        sc.close();
+    }
+}
