d8b165f3a631edbb16bcad345a73d63aa75c8636
Vaishali Makwana <vaishalimakwana200725@gmail.com>
2026-08-16 21:10:46 +0530

    Create SumOfDigits.java

---
+++ SumOfDigits.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int sum = 0;
+        int temp = num;
+
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum = sum + digit;
+            temp = temp / 10;
+        }
+
+        System.out.println("Sum of digits: " + sum);
+    }
+}
